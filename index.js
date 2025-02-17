cb5abb104102c1e4ee63c4b13fb084299f7c5c59
satyammthakur <satyammthakur@gmail.com>
2025-02-17 19:52:20 +0530

    added addBlog feature and then added the feature to show individual blogs and also implemented multer to store files in the upload section

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ import 'dotenv/config';
 import { connectToMongoDB } from "./connect.js";
 import cookieParser from "cookie-parser";
 import userRoute from "./routes/user.routes.js";
+import blogRoute from "./routes/blog.routes.js";
 import { checkForAuthenticationCookie } from "./middlewares/auth.middleware.js";
-
+import Blog from "./models/blog.models.js";
 const app = express();
 const PORT = 8000;
 
@@ -15,13 +16,17 @@ app.set("views" , path.resolve("./views"))
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')));
 
-app.get('/' , (req,res)=>{
+app.get('/' , async (req,res)=>{
+    const allBlogs = await Blog.find({});
     res.render('home' ,{
         user:req.user,
+        blogs: allBlogs,
     });
 })
 app.use("/user" , userRoute);
+app.use("/blog" , blogRoute);
 connectToMongoDB(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.r5pa8.mongodb.net/${process.env.MONGO_DBNAME}retryWrites=true&w=majority`)
 .then(()=>{
     console.log("connected to the db");
---
+++ routes/blog.routes.js
@@ -0,0 +1,46 @@
+import { Router } from "express";
+import multer from "multer";
+import path from "path";
+import Blog from "../models/blog.models.js";
+const router = Router();
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/`));
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`;
+      cb(null,fileName);
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+router.get('/add-new' , (req,res)=>{
+    return res.render("addBlog" ,{
+        user: req.user,
+    });
+})
+router.get("/:id" , async(req,res)=>{
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    return res.render('blog' ,{
+        user: req.user,
+        blog,
+    });
+})
+router.post('/' , upload.single("coverImage"), (req,res)=>{
+    // console.log(req.body);
+    // console.log(req.file);
+    const {title , body} = req.body;
+    const blog = Blog.create({
+        body,
+        title,
+        createdBy: req.user._id,
+        coverImageURL: `/uploads/${req.file.filename}`,
+    })
+   // return res.render("addBlog" ,{
+    //     user: req.user,
+    // });
+    return res.redirect(`/blog/${blog._id}`);
+})
+
+export default router;
